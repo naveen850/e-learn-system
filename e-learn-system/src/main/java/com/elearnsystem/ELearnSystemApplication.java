922dc539e75b6adf5257caa2ef470179ddad49bb
welcome <welcome@192.168.11.28>
2019-10-30 17:58:18 +0530

    commit by naveen

---
+++ e-learn-system/src/main/java/com/elearnsystem/ELearnSystemApplication.java
@@ -0,0 +1,15 @@
+package com.elearnsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.elearnsystem.ELearnSystemApplication;
+@SpringBootApplication
+public class ELearnSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELearnSystemApplication.class, args);
+
+	}
+
+}
---
+++ e-learn-system/src/main/java/com/elearnsystem/ServletInitialize.java
@@ -0,0 +1,15 @@
+package com.elearnsystem;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+
+public class ServletInitialize extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ELearnSystemApplication.class);
+
+}
+}
